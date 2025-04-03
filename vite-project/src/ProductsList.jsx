8df187695e5b20664644f9dd0f78d5c6be248518
adrianopiacentini <adrianopiacentini03@gmail.com>
2025-04-03 14:25:49 +0200

    Milestone 3 completata

--- vite-project/src/ProductsList.jsx
+++ vite-project/src/ProductsList.jsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function ProductsList() {
     const [addedProducts, setAddedProducts] = useState([])
+    const [totalPrice, setTotalPrice] = useState(0)
 
     const products = [
         { name: 'Mela', price: 0.5 },
@@ -19,9 +20,34 @@ function ProductsList() {
                 name: product.name, price: product.price, quantity: 1
             }])
         } else {
-            alert(`${product.name} è già nel carrello!`)
+            updateProductQuantity(product)
         }
     }
+
+    const removeFromCart = (product) => {
+        setAddedProducts((prevProducts) => prevProducts.filter((addedProducts) => addedProducts.name !== product.name))
+    }
+
+    const updateProductQuantity = (product) => {
+        setAddedProducts((prevProducts) => prevProducts.map((addedProduct) => addedProduct.name === product.name
+            ? { ...addedProduct, quantity: addedProduct.quantity++ }
+            : addedProduct
+        ))
+    }
+
+    useEffect(() => {
+        const calcPrice = () => {
+            let newPrice = 0;
+            addedProducts.forEach((product) => {
+                newPrice += (product.price * product.quantity)
+            })
+            setTotalPrice(newPrice.toFixed(2))
+        }
+
+        calcPrice()
+    }, [addedProducts])
+
+
 
     return (
         <>
@@ -42,12 +68,15 @@ function ProductsList() {
                         {addedProducts.map(product => (
                             <li key={product.name}>
                                 <p>Nome: {product.name}, Prezzo: {product.price}, Quantità: {product.quantity}</p>
+                                <button onClick={() => removeFromCart(product)}>Rimuovi dal carrello</button>
                             </li>
                         ))}
                     </ul>
                 )
 
                 }
+
+                <h2>Totale da pagare: {totalPrice}</h2>
 
             </ul>
         </>
